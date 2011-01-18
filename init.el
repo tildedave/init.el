60af73a0a844151793d955c81ef1acde703aabf7
Dave King <tildedave@gmail.com>
2011-01-18 06:46:42 -0500

    refactor of .emacs.d/init.el

--- init.el
+++ init.el
@@ -1,52 +1,107 @@
-(load "~/.emacs.d/haskell-mode-2.8.0/haskell-site-file.el")
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-(add-hook 'haskell-mode-hook 'font-lock-mode)
-(add-to-list 'load-path "~/.emacs.d/")
+;; Basic setup
+(setq my-emacsd (file-name-as-directory "~/.emacs.d"))
+(setq my-site-lisp my-emacsd)
+(add-to-list 'load-path my-site-lisp)
 
-;;(load-file "~/.emacs.d/hilit19.el")
-;;(load-file "~/.emacs.d/coq.el")
-;;(load-file "~/.emacs.d/coq-inferior.el")
+;; Which language modes get loaded
+(setq use-haskell t)
+(setq use-geiser t)
+(setq use-proofgeneral t)
+(setq use-js2 t)
+(setq use-yaml t)
 
-;;(defvar coq-prog-name "coq-emacs.bat")
-(load-file "~/.emacs.d/ProofGeneral-4.0pre100927/generic/proof-site.el")
-;;(load-file "~/.emacs.d/js2.elc")
-(autoload 'js2-mode "js2" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-
-;; (cond (window-system
-;;       (setq hilit-mode-enable-list  '(not text-mode)
-;;             hilit-background-mode   'light
-;;             hilit-inhibit-hooks     nil
-;;             hilit-inhibit-rebinding nil)
-;;       (require 'hilit19)
-;;       ))
-
-;(global-set-key (kbd "<C-tab>") 'bury-buffer)
-(global-set-key (kbd "<C-tab>") 'other-window)
-;(global-set-key (kbd "<S-C-tab>") 'previous-buffer)
+;; Essential keybindings
 (global-set-key "\C-x\C-b" 'electric-buffer-list)
+(global-set-key (kbd "<C-tab>") 'other-window)
+(global-set-key "\C-f" 'scroll-down)
+(global-set-key "\M-g" 'goto-line)
+
+;; Check if we are on my Windows 7 box
+(setq is-windows (string= system-type "windows-nt"))
+
+;; Directory Conversion Functions
+
+(defun list-to-directory (list)
+  (let (value)
+    (dolist (element list value)
+      (setq value (file-name-as-directory (concat value element))))))
+
+(defun path-to-file (list filename) 
+  (concat (list-to-directory list) filename))
+
+;; LANGUAGE SPECIFIC MODES 
+
+;; Haskell
+
+(if use-haskell
+    (let
+        ((haskell-site-file
+          (path-to-file (list my-site-lisp "haskell-mode-2.8.0") "haskell-site-file.el")))
+      (if (file-exists-p haskell-site-file)
+          (progn
+            (load haskell-site-file)
+            (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+            (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+            (add-hook 'haskell-mode-hook 'font-lock-mode))
+        (warn "Error loading Haskell mode -- could not find site file"))))
+
+;; Coq/Proof General
+
+(if use-proofgeneral
+    (let ((proofgeneral-site-file
+           (path-to-file (list my-emacsd "ProofGeneral-4.0pre100927" "generic") "proof-site.el")))
+      (if (file-exists-p proofgeneral-site-file)
+          (load-file proofgeneral-site-file)
+        (warn "Error loading Proof General mode -- could not find site file"))))
+
+;; Geiser Mode
+(if use-geiser
+    (let 
+        ((geiser-load-file 
+          (path-to-file (list my-site-lisp "geiser" "build" "elisp") "geiser-load.elc"))
+         )
+      (if (file-exists-p geiser-load-file)
+          (progn
+            (load geiser-load-file)
+            (require 'geiser-install)
+            (if is-windows
+                (setq geiser-scheme-dir "C:/cygwin/usr/local/share/geiser")))
+        (warn "Unable to load geiser-mode"))))
+
+;; JS2/Yaml -- These should be in the load path
+
+(if use-js2
+    (progn
+      (autoload 'js2-mode "js2" nil t)
+      (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))))
+
+(if use-yaml
+    (progn 
+      (require 'yaml-mode)
+      (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))))
+
+
+;; QUALITY OF LIFE SETTINGS
+
+;; savehist mode
 (savehist-mode 1)
 (setq savehist-additional-variables '(kill-ring search-ring regexp-search-ring))
-(setq savehist-file "~/.emacs.d/tmp/savehist")
+(setq savehist-file (concat (file-name-as-directory (concat my-emacsd "tmp")) "savehist"))
 
-(add-hook 'comint-output-filter-functions
-    'shell-strip-ctrl-m nil t)
-(add-hook 'comint-output-filter-functions
-    'comint-watch-for-password-prompt nil t)
-(setq explicit-shell-file-name "bash.exe")
-;; For subprocesses invoked via the shell
-;; (e.g., "shell -c command")
+(if is-windows 
+    (progn 
+      (add-hook 'comint-output-filter-functions
+                'shell-strip-ctrl-m nil t)
+      (add-hook 'comint-output-filter-functions
+                'comint-watch-for-password-prompt nil t)
+      (setq explicit-shell-file-name "bash.exe")))
 
-(load "~/.emacs.d/geiser/build/elisp/geiser-load")
-(require 'geiser-install)
-(setq geiser-scheme-dir "C:/cygwin/usr/local/share/geiser")
-
+  
+;; Shell coloring
 (setq shell-file-name explicit-shell-file-name)
 (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
+;; No tabs
 (setq-default indent-tabs-mode nil)
 
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
